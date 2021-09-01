865879155dab9be5475ca6165795468c0322ecaf
naruaki763 <narusiotau763@gmail.com>
2021-09-01 16:45:01 +0900

    add new file

---
+++ event.js
@@ -0,0 +1,14 @@
+﻿chrome.alarms.onAlarm.addListener((alarm) => {
+  //alert('Alret');
+
+  chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+    chrome.tabs.reload(arrayOfTabs[0].id);
+});
+
+  chrome.windows.create({
+    width : 200,
+    height : 100,
+    type : 'popup',
+    url : 'alert.html'
+  });
+});
---
+++ myscript.js
@@ -0,0 +1,21 @@
+﻿
+
+function valueChange(event){
+    // イベントが発生した時の処理
+    if (mycheck.checked){
+      // チェックあり の場合
+      chrome.alarms.create('rilo', { delayInMinutes : 1, periodInMinutes : 1 });
+      chrome.alarms.onAlarm.addListener(function(alarm) {
+    
+        if (alarm.name == 'rilo') {
+         chrome.runtime.reload();
+          };
+        
+});
+    } else {
+      // チェックなし の場合
+        alert("テスト2");
+    }
+}
+let saveCheckbox = document.getElementById('mycheck');
+saveCheckbox.addEventListener('change', valueChange);
