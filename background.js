8d2bcd5c0bed6d6488b9aebce164c75ace1bd2b8
naruaki763 <89901095+naruaki763@users.noreply.github.com>
2021-10-06 15:55:33 +0900

    Update background.js

--- background.js
+++ background.js
@@ -4,3 +4,4 @@ funciton profile(){
   
     document.getElementById("fr").contentWindow.location.replace('https://ksmapy.kokugakuin.ac.jp/up/faces/up/jg/Jgc00101A.jsp');
     document.getElementById("fr").contentWindow.document.getElementByID("forml:linkClassProfile").click();
+}
